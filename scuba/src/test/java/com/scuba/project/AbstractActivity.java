4fa6747b5e7b5c49f31f4e37da284cbc3887e476
Kandharaja M <kandharajam@maveric-systems.com>
2023-02-25 22:28:42 +0530

    subject

---
+++ scuba/src/test/java/com/scuba/project/AbstractActivity.java
@@ -0,0 +1,36 @@
+package com.scuba.project;
+
+public class AbstractActivity {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+For (inti=0:i<n;i++)
+
+}
+abstract class Sunstar {
+    abstract void printInfo();
+}
+ 
+class Employee extends Sunstar {
+    void printInfo() {
+        String name = "Raja";
+        int age = 21;
+        float salary = 222.2F;
+ 
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(salary);
+ 
+    }
+ 
+}
+ 
+class Base {
+    public static void main(String args[]) {
+        Sunstar s = new Employee();
+        s.printInfo();
+    }
+}
